bd2878a87a3f97d9ca4d735e9c1c40abca335782
Sushil <apple@Apples-MacBook-Pro.local>
2025-06-28 13:50:38 +0530

    menu colour and width changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,13 @@
 // src/components/Header.jsx
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.jpg";
 
 export default function Header() {
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+  const toggleNavbar = () => setIsNavCollapsed(!isNavCollapsed);
+
   return (
     <nav className="navbar navbar-expand-lg bg-brand-purple text-brand-gold px-4">
       <div className="container-fluid">
@@ -13,7 +18,22 @@ export default function Header() {
           <img src={logo} alt="Logo" className="me-2 rounded logo-img" />
           <strong>Laxmi Printing Press</strong>
         </Link>
-        <div className="collapse navbar-collapse">
+        <button
+          className="navbar-toggler"
+          type="button"
+          aria-controls="navbarNav"
+          aria-expanded={!isNavCollapsed}
+          aria-label="Toggle navigation"
+          onClick={toggleNavbar}
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className={`collapse navbar-collapse ${
+            !isNavCollapsed ? "show" : ""
+          }`}
+          id="navbarNav"
+        >
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
               <Link className="nav-link text-white" to="/">

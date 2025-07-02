83174607d7d9084a3c52b99068893f9375bd509c
Sushil <apple@Apples-MacBook-Pro.local>
2025-07-02 22:53:10 +0530

    map change: location

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,15 +4,26 @@ export default function Contact() {
     <div className="container-fluid py-4">
       <div className="container wide-container">
         <h2 className="mb-4">Contact Us</h2>
-        <p><strong>Location:</strong> Bhagat Singh Chowk, Lalbagh, Darbhanga</p>
-        <p><strong>Phone/WhatsApp:</strong> <a href="https://wa.me/6207871803" target="_blank" rel="noopener noreferrer">6207871803</a></p>
+        <p>
+          <strong>Location:</strong> Bhagat Singh Chowk, Lalbagh, Darbhanga
+        </p>
+        <p>
+          <strong>Phone/WhatsApp:</strong>{" "}
+          <a
+            href="https://wa.me/6207871803"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            6207871803
+          </a>
+        </p>
 
         <div className="mt-4">
           <iframe
             title="Laxmi Printing Press Map"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3608.3120679984253!2d85.90260127453366!3d26.153782877110483!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39edb82785b3b2c7%3A0xa8bbfd2e2804dc93!2sBhagat%20Singh%20Chowk%2C%20Darbhanga%2C%20Bihar%20846004!5e0!3m2!1sen!2sin!4v1719393650576!5m2!1sen!2sin"
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d895.3237194080549!2d85.8929377189102!3d26.15452436970587!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39edb9853d0b1d5d%3A0x2048cb798527bcb4!2sLaxmi%20Printing%20Press!5e0!3m2!1sen!2sin!4v1751476903597!5m2!1sen!2sin"
             width="100%"
-            height="350"
+            height="450"
             style={{ border: 0 }}
             allowFullScreen=""
             loading="lazy"
